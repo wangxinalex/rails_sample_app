cd39092d44d949f7aa6d21932247af4968b7eac2
wangxinalex <wangxinalex@gmail.com>
2014-02-25 00:10:06 -0800

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,42 +1,38 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+	subject {page}
 	describe "Home Page" do
-    	it "should have the content 'Sample App'" do
-      	# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-	  		visit '/static_pages/home'
-	  		expect(page).to have_content('Sample App')
-    	end
-
-		it "should have the base title" do
-			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-		end
-
-		it "should not have a custom page title" do
-			visit '/static_pages/home'
-			expect(page).not_to have_title('|Home')
-		end
+		before {visit root_path}
+		it {should have_content('Sample App')}
+		it {should have_title(full_title(''))}
+		it {should_not have_title('| Home')}
 	end
 
 	describe "Help page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('Help')
 		end
 	end
 
 	describe "About page" do
 		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About Us')
 		end
 	end
 
 	describe "Contact page" do
 		it "should have the content 'Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_content('Contact Us')
 		end
+
+		it "should have the title 'Contact'" do
+			visit contact_path
+			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
+		end
+
 	end
 end
