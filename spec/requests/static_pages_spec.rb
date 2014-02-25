0be6828249c86df9ab75560f96a29168fed6c1e1
wangxinalex <wangxinalex@gmail.com>
2014-02-25 00:29:53 -0800

    Chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,18 +2,25 @@ require 'spec_helper'
 
 describe "Static Pages" do
 	subject {page}
+
+	shared_examples_for "all static pages" do
+		it {should have_content(heading)}
+		it {should have_title(full_title(page_title))}
+	end
+
 	describe "Home Page" do
 		before {visit root_path}
-		it {should have_content('Sample App')}
-		it {should have_title(full_title(''))}
+		let(:heading) {'Sample App'}
+		let(:page_title) {''}
+		it_should_behave_like "all static pages"
 		it {should_not have_title('| Home')}
 	end
 
 	describe "Help page" do
-		it "should have the content 'Help'" do
-			visit help_path
-			expect(page).to have_content('Help')
-		end
+		before {visit help_path}
+		let(:heading) {'Help'}
+		let(:page_title) {''}
+		it_should_behave_like "all static pages"	
 	end
 
 	describe "About page" do
@@ -34,5 +41,12 @@ describe "Static Pages" do
 			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
 		end
 
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"	
+		expect(page).to have_title(full_title('About Us'))
+		
 	end
 end
