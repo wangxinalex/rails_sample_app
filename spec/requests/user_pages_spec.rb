6c7cf4b0e74a9d6f01f638f0eeaa3a935d8f6dce
wangxinalex <wangxinalex@gmail.com>
2014-03-02 23:49:54 -0800

    deleted *.swo files

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,8 +20,15 @@ describe "User Pages" do
 		before {visit signup_path}
 		let(:submit) {"Create my account"}
 		describe "with invalid information" do
+			
 			it "should not create a user" do
 				expect {click_button submit }.not_to change(User, :count)
+			end
+			
+			describe "after submission" do
+				before{click_button submit}
+				it{should have_title('Sign up')}
+				it{should have_content('error')}
 			end
 		end
 
@@ -35,6 +42,13 @@ describe "User Pages" do
 
 			it "should create a user" do
 				expect {click_button submit}.to change(User, :count).by(1)
+			end
+			
+			describe "after saving the user" do
+				before {click_button submit}
+				let(:user) {User.find_by(email:'user@example')}
+				#it{should have_title(user.name)}
+				it{should have_selector('div.alert.alert-success', text:'Welcome')}
 			end
 		end
 	end
